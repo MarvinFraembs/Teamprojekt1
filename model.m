70f131934b8f32a0c679a2f8f2f69f3e7580d675
Drawfiber <115083708+Drawfiber@users.noreply.github.com>
2024-12-09 17:59:51 +0100

    Optimaler Volumenstrom und Plot

--- model.m
+++ model.m
@@ -7,11 +7,7 @@ p.k2 = 0.007;                   % 1/s
 p.k3 = 0.0012;                  % 1/s
 p.V = 0.1;                      % m^3
 p.cA_in = 1000;                 % mol/m^3
-%p.F_in = [10000/3600, 1000/3600, 100/3600, 10/3600];
-p.F1_in = 10000/(3600*1000);    % m^3/s
-% p.F2_in = 1000/3600; 
-% p.F3_in = 100/3600;
-% p.F4_in = 10/3600;
+volumenstroeme = [10000, 1000, 100, 10] / (3600 * 1000); % Liste der Volumenströme (m^3/s)
 
 %%Solverparameter 
 y0 = [p.cA_in 0 0]; % [cA_in cR_in cS_in]
@@ -20,8 +16,39 @@ tspanbatch = [0 2000];
 % tspan = 0:0.01:50;
 option = odeset;
 
-%%Solver
-[t_cstr, y_cstr] = ode45(@F1_cstr, tspancstr, y0, option, p); 
+%% Optimaler Volumenstrom
+
+% Speicher für Ergebnisse
+max_cR_values = []; % Maxima von cR
+optimal_F = [];     % Volumenstrom mit dem größten Maximum von cR
+
+% Simulation für jeden Volumenstrom
+for i = 1:length(volumenstroeme)
+    p.F1_in = volumenstroeme(i); % Setzen des aktuellen Volumenstroms
+    [t_cstr, y_cstr] = ode45(@F1_cstr, tspancstr, y0, option, p); % Simulation
+    
+    cR = y_cstr(:, 2); % Konzentration cR extrahieren
+    [cR_max, idx_max] = max(cR); % Maximum von cR finden
+    max_cR_values(end + 1) = cR_max; % Speichern des Maximums
+    
+    % Ergebnis ausgeben
+    fprintf('Volumenstrom F = %.10f m^3/s, Maximum von cR = %.5f mol/m^3\n', ...
+            volumenstroeme(i), cR_max);
+end
+
+% Optimalen Volumenstrom finden
+[optimal_cR_max, optimal_idx] = max(max_cR_values);
+optimal_F = volumenstroeme(optimal_idx);
+
+%% Simulation mit dem optimalen Volumenstrom
+p.F1_in = optimal_F; % Setzen des optimalen Volumenstroms
+[t_cstr_opt, y_cstr_opt] = ode45(@F1_cstr, tspancstr, y0, option, p);
+
+% Ergebnis ausgeben
+fprintf('\nDer optimale Volumenstrom ist F = %.10f m^3/s mit cR_max = %.5f mol/m^3.\n', ...
+        optimal_F, optimal_cR_max);
+
+%%Solver Batch Reaktor 
 [t_batch, y_batch] = ode45(@F1_batch, tspanbatch, y0, option, p);
 
 %%Optimale Verweilzeit Batch Reaktor
@@ -39,9 +66,9 @@ figure; % Neues Fenster für das Plot
 hold on; % Alle Graphen im selben Fenster
 
 % Konzentration von A, R und S über der Zeit plotten
-plot(t_cstr, y_cstr(:, 1), 'r', 'LineWidth', 2); % cA in rot
-plot(t_cstr, y_cstr(:, 2), 'g', 'LineWidth', 2); % cR in grün
-plot(t_cstr, y_cstr(:, 3), 'b', 'LineWidth', 2); % cS in blau
+plot(t_cstr_opt, y_cstr_opt(:, 1), 'r', 'LineWidth', 2); % cA in rot
+plot(t_cstr_opt, y_cstr_opt(:, 2), 'g', 'LineWidth', 2); % cR in grün
+plot(t_cstr_opt, y_cstr_opt(:, 3), 'b', 'LineWidth', 2); % cS in blau
 
 % Achsenbeschriftungen und Titel
 xlabel('Zeit (s)', 'FontSize', 12);
