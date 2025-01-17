aa8a786d85315854aa948c1802f796d87aa68989
Drawfiber <115083708+Drawfiber@users.noreply.github.com>
2025-01-17 16:39:57 +0100

    F1_ready

--- F1_cstr.m
+++ F1_cstr.m
@@ -8,12 +8,12 @@ function [dydt] = F1_cstr(t,y,p)
     r2 = cR * p.k2;
     r3 = cA * p.k3;
     
-    dTdt = (F1_in/p.V)*()
+    dTdt = (F1_in/p.V)*(p.cA_in*p.Cp_A/(p.cA*p.Cp_A+p.cR*p.Cp_R+p.cS*p.Cp_S))*(p.T_in-p.T_Oel1)-(p.k3*p.dh_3)/(p.cA*p.Cp_A+p.cR*p.Cp_R+p.cS*p.Cp_S)+(p.dh_3/p.V)*1/(p.cA*p.Cp_A+p.cR*p.Cp_R+p.cS*p.Cp_S);
     dcAdt = p.F1_in/p.V * (p.cA_in - cA) - r1 + r2 - 2 * r3;
     dcRdt = -p.F1_in/p.V * cR + r1 - r2;
     dcSdt = -p.F1_in/p.V * cS + 2 * r3;
 
     disp(['Zeit: ', num2str(t), ' cA: ', num2str(cA)]);
 
-    dydt = [dcAdt; dcRdt; dcSdt];
+    dydt = [dTdt; dcAdt; dcRdt; dcSdt];
 end
--- model.m
+++ model.m
@@ -2,21 +2,29 @@ clc;
 clear all;
 close all;
 
+%CSTR
 p.F1_in = 1000/(3600*1000) % m^3/s
 p.A = 6           % m^2
 p.V = 0.1         % m^3
-p.m_ks = 100      % kg
 p.Tau = 300       % s
 p.T_in = 293.15   % K
 p.T_Oel1 = 353.15 % K
-p.T_Oel2 = 350    % K
-p.Cp_ks = 2500    % J/kg*K
 p.Cp_A = 2300     % J/mol*K
 p.Cp_R = 2100     % J/mol*K
 p.Cp_S = 2700     % J/mol*K
 p.dh_1 = 10000    % J/mol
 p.dh_2 = -10000   % J/mol
 p.dh_3 = 14000    % J/mol
+p.k1 = 0.007      % 1/s
+p.k1 = 0.007      % 1/s
+p.k1 = 0.0012     % 1/s
+p.cA_in = 1000    % mol/m^3
+
+%Prozessidentifikation
+
+p.m_ks = 100      % kg
+p.T_Oel2 = 350    % K
+p.Cp_ks = 2500    % J/kg*K
 p.kA = 1250/9     % W/(m^2*K)
 
 %% Solver Prozessidentifikation
