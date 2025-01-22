fdc5c2f4662f3a4761d976cea9a30ddbc9ff904c
Marvin <marvin.fraembs241@gmail.com>
2025-01-22 17:57:11 +0100

    added plot

--- F1_cstr.m
+++ F1_cstr.m
@@ -9,7 +9,7 @@ function [dydt] = F1_cstr(t,y,p)
     r2 = cR * p.k2;
     r3 = cA * p.k3;
     
-    dTdt = (p.F1_in/p.V)*(p.cA_in*p.Cp_A/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S))*(p.T_in-p.T_Oel1)-(p.k3*p.dh_3)/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S)+(p.Q/p.V)*1/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S);
+    dTdt = (p.F1_in/p.V)*(p.cA_in*p.Cp_A/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S))*(p.T_in-p.T_Oel1)-(r3*p.dh_3)/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S)-(r2*p.dh_2)/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S)-(r1*p.dh_1)/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S)+(p.Q/p.V)*1/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S);
     dcAdt = p.F1_in/p.V * (p.cA_in - cA) - r1 + r2 - 2 * r3;
     dcRdt = -p.F1_in/p.V * cR + r1 - r2;
     dcSdt = -p.F1_in/p.V * cS + 2 * r3;
--- model.m
+++ model.m
@@ -45,8 +45,9 @@ hold on; % Alle Graphen im selben Fenster
 
 % Konzentration von A, R und S über der Zeit plotten
 plot(t, y(:, 1), 'r', 'LineWidth', 2); % cA in rot
-%plot(t, y(:, 2), 'g', 'LineWidth', 2); % cR in grün
-%plot(t, y(:, 3), 'b', 'LineWidth', 2); % cS in blau
+plot(t, y(:, 2), 'g', 'LineWidth', 2); % cR in grün
+plot(t, y(:, 3), 'b', 'LineWidth', 2); % cS in blau
+plot(t, y(:, 4), 'y', 'LineWidth', 2);
 
 % Achsenbeschriftungen und Titel
 xlabel('Zeit (s)', 'FontSize', 12);
