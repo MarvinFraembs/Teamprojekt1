d2d00ccc8c6fdb9e2cf972f988b84d862472a46d
Marvin <marvin.fraembs241@gmail.com>
2025-01-22 17:05:03 +0100

    second commit

--- F1_cstr.m
+++ F1_cstr.m
@@ -1,14 +1,15 @@
-function [dydt] = F1_cstr(t,y,p)
+function [dydt] = F1(t,y,p)
 
-    cA = y(1);
-    cR = y(2);
-    cS = y(3);
+    T = y(1);
+    cA = y(2);
+    cR = y(3);
+    cS = y(4);
 
     r1 = cA * p.k1;
     r2 = cR * p.k2;
     r3 = cA * p.k3;
     
-    dTdt = (F1_in/p.V)*(p.cA_in*p.Cp_A/(p.cA*p.Cp_A+p.cR*p.Cp_R+p.cS*p.Cp_S))*(p.T_in-p.T_Oel1)-(p.k3*p.dh_3)/(p.cA*p.Cp_A+p.cR*p.Cp_R+p.cS*p.Cp_S)+(p.dh_3/p.V)*1/(p.cA*p.Cp_A+p.cR*p.Cp_R+p.cS*p.Cp_S);
+    dTdt = (p.F1_in/p.V)*(p.cA_in*p.Cp_A/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S))*(p.T_in-p.T_Oel1)-(p.k3*p.dh_3)/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S)+(p.dh_3/p.V)*1/(cA*p.Cp_A+cR*p.Cp_R+cS*p.Cp_S);
     dcAdt = p.F1_in/p.V * (p.cA_in - cA) - r1 + r2 - 2 * r3;
     dcRdt = -p.F1_in/p.V * cR + r1 - r2;
     dcSdt = -p.F1_in/p.V * cS + 2 * r3;
--- model.m
+++ model.m
@@ -3,57 +3,61 @@ clear all;
 close all;
 
 %CSTR
-p.F1_in = 1000/(3600*1000) % m^3/s
-p.A = 6           % m^2
-p.V = 0.1         % m^3
-p.Tau = 300       % s
-p.T_in = 293.15   % K
-p.T_Oel1 = 353.15 % K
-p.Cp_A = 2300     % J/mol*K
-p.Cp_R = 2100     % J/mol*K
-p.Cp_S = 2700     % J/mol*K
-p.dh_1 = 10000    % J/mol
-p.dh_2 = -10000   % J/mol
-p.dh_3 = 14000    % J/mol
-p.k1 = 0.007      % 1/s
-p.k1 = 0.007      % 1/s
-p.k1 = 0.0012     % 1/s
-p.cA_in = 1000    % mol/m^3
+p.F1_in = 1000/(3600*1000); % m^3/s
+p.A = 6;           % m^2
+p.V = 0.1;         % m^3
+p.Tau = 300;       % s
+p.T_in = 293.15;   % K
+p.T_Oel1 = 353.15; % K
+p.Cp_A = 2300;     % J/mol*K
+p.Cp_R = 2100;     % J/mol*K
+p.Cp_S = 2700;     % J/mol*K
+p.dh_1 = 10000;    % J/mol
+p.dh_2 = -10000;   % J/mol
+p.dh_3 = 14000;    % J/mol
+p.k1 = 0.007;      % 1/s
+p.k2 = 0.007;      % 1/s
+p.k3 = 0.0012;     % 1/s
+p.cA_in = 1000;    % mol/m^3
 
 %Prozessidentifikation
 
-p.m_ks = 100      % kg
-p.T_Oel2 = 350    % K
-p.Cp_ks = 2500    % J/kg*K
-p.kA = 1250/9     % W/(m^2*K)
+p.m_ks = 100;      % kg
+p.T_Oel2 = 350;    % K
+p.Cp_ks = 2500;    % J/kg*K
+p.kA = 1250/9;     % W/(m^2*K)
 
 %% Solver Prozessidentifikation
 
 %Solverparameter
-y0 = [p.T_in]; % [T_in]
-tspanident = [0 500];
+y0 = [p.T_in, p.cA_in, 0, 0]; % [T_in]
+tspan = [0 500];
 option = odeset;
 
+[t, y] = ode45(@(t, y) F1_cstr(t, y, p), tspan, y0);
+
+
+
 %% Plot
-figure; % Neues Fenster für das Plot
-hold on; % Alle Graphen im selben Fenster
+%figure; % Neues Fenster für das Plot
+%hold on; % Alle Graphen im selben Fenster
 
 % Konzentration von A, R und S über der Zeit plotten
-plot(t, y(:, 1), 'r', 'LineWidth', 2); % cA in rot
-plot(t, y(:, 2), 'g', 'LineWidth', 2); % cR in grün
-plot(t, y(:, 3), 'b', 'LineWidth', 2); % cS in blau
+%%plot(t, y(:, 1), 'r', 'LineWidth', 2); % cA in rot
+%plot(t, y(:, 2), 'g', 'LineWidth', 2); % cR in grün
+%plot(t, y(:, 3), 'b', 'LineWidth', 2); % cS in blau
 
 % Achsenbeschriftungen und Titel
-xlabel('Zeit (s)', 'FontSize', 12);
-ylabel('Konzentration (mol/m^3)', 'FontSize', 12);
-title('Konzentrationsverläufe der Spezies A, R und S', 'FontSize', 14);
+%xlabel('Zeit (s)', 'FontSize', 12);
+%ylabel('Konzentration (mol/m^3)', 'FontSize', 12);
+%title('Konzentrationsverläufe der Spezies A, R und S', 'FontSize', 14);
 
 % Legende hinzufügen
-legend('cA (rot)', 'cR (grün)', 'cS (blau)', 'Location', 'northeast');
+%legend('cA (rot)', 'cR (grün)', 'cS (blau)', 'Location', 'northeast');
 
 % Gitter anzeigen
-grid on;
+%grid on;
 
-hold off; % Plot beenden
+%hold off; % Plot beenden
 
 %%Plots
