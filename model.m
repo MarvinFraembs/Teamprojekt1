ef60dbd2ace5b304f3bbf904d4257cb6db0e45fb
Marvin <marvin.fraembs241@gmail.com>
2025-01-22 17:39:49 +0100

    added plot

--- model.m
+++ model.m
@@ -40,25 +40,24 @@ option = odeset;
 
 
 %% Plot
-%figure; % Neues Fenster für das Plot
-%hold on; % Alle Graphen im selben Fenster
+figure; % Neues Fenster für das Plot
+hold on; % Alle Graphen im selben Fenster
 
 % Konzentration von A, R und S über der Zeit plotten
-%%plot(t, y(:, 1), 'r', 'LineWidth', 2); % cA in rot
+plot(t, y(:, 1), 'r', 'LineWidth', 2); % cA in rot
 %plot(t, y(:, 2), 'g', 'LineWidth', 2); % cR in grün
 %plot(t, y(:, 3), 'b', 'LineWidth', 2); % cS in blau
 
 % Achsenbeschriftungen und Titel
-%xlabel('Zeit (s)', 'FontSize', 12);
-%ylabel('Konzentration (mol/m^3)', 'FontSize', 12);
+xlabel('Zeit (s)', 'FontSize', 12);
+xlim([0 5000]);
+ylabel('Temperatur (Kelvin)/ Konzentration (mol/m^3)', 'FontSize', 12);
 %title('Konzentrationsverläufe der Spezies A, R und S', 'FontSize', 14);
 
 % Legende hinzufügen
 %legend('cA (rot)', 'cR (grün)', 'cS (blau)', 'Location', 'northeast');
 
 % Gitter anzeigen
-%grid on;
+grid on;
 
-%hold off; % Plot beenden
-
-%%Plots
+hold off; % Plot beenden
